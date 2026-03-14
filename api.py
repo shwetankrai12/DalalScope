313684e8c99858d09df8a813ed7faf345ba692f4
shwetankrai12 <shwetankrai12102004@gmail.com>
2026-03-15 00:34:39 +0530

    Add Procfile for deployment

--- api.py
+++ api.py
@@ -85,3 +85,9 @@ async def resolve_company(company: str):
 
 
 app.mount("/", StaticFiles(directory="static", html=True), name="static")
+
+if __name__ == "__main__":
+    import uvicorn
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("api:app", host="0.0.0.0", port=port)
